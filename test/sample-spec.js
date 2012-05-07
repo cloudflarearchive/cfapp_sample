fe5bc807019e3f0210d0533f6941b36898cd1a2b
Michael Sofaer <michael@cloudflare.com>
2012-05-07 15:16:38 -0700

    First test of the json

--- test/sample-spec.js
+++ test/sample-spec.js
@@ -3,3 +3,9 @@ describe(["sample_app"], function(sample){
         expect(sample.name).toEqual("Sample App");
     })
 })
+
+describe(["app_json"], function(json){
+    it("should have a name", function(){
+        expect(json.name).toEqual("Sample App")
+    })
+})
