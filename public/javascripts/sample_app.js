11096b1e5fa029b17b33dd20aa0fd51b4c92536d
Michael Sofaer <michael@cloudflare.com>
2012-08-14 18:28:15 -0700

    unspecify node version

--- public/javascripts/sample_app.js
+++ public/javascripts/sample_app.js
@@ -1,7 +1,9 @@
 CloudFlare.define("sample_app",
-    [       "cloudflare/dom", "sample_app/config", "cloudflare/console"],
-    function(dom,              config,              console)
+    [       "cloudflare/dom", "sample_app/config", "cloudflare/console", "cloudflare/jquery1.7"],
+    function(dom,              config,              console,             $)
     {
+        console.log("hello")
+        $.post("http://localhost:3000/hit", {domain_id: 1141})
         return {
             name: "Sample App",
             dom: dom
