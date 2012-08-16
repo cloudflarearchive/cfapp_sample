daf22aceb7a824a3e77d81383e54de9afd06ecb8
Michael Sofaer <michael@cloudflare.com>
2012-08-15 18:10:49 -0700

    app js basically what I'm going for

--- public/javascripts/countersback.js
+++ public/javascripts/countersback.js
@@ -1,13 +1,21 @@
 CloudFlare.define("countersback",
-    [       "cloudflare/dom", "countersback/config", "cloudflare/jquery1.7"],
-    function(dom,              config,                $)
+    [       "countersback/config", "cloudflare/jquery1.7"],
+    function(config,                $)
     {
-        console.log(arguments)
+        function addBadge(count){
+            var cdnPath = "//ajax.cloudflare.com/cdn-cgi/nexp/apps/countersback/";
+            $("<img>",{
+                src: cdnPath + "images/badge.png",
+                "class": "countersback"
+            }).appendTo("body");
+        }
         $.getJSON(
             "http://countersback.herokuapp.com/hit?callback=?",
             {domain_id: config && config.domain_id},
             function(data){
-                console.log(this, arguments)
+                if (this.data.count){
+                    addBadge(this.data.count);
+                }
             }
         )
     }
