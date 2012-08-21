cf109ead23e47d486ff202abc3cdf8b14f488868
Michael Sofaer <michael@cloudflare.com>
2012-08-21 12:27:30 -0700

    Add a live image

--- public/javascripts/countersback.js
+++ public/javascripts/countersback.js
@@ -4,17 +4,21 @@ CloudFlare.define("countersback",
     {
         function addBadge(count){
             var cdnPath = "//ajax.cloudflare.com/cdn-cgi/nexp/apps/countersback/";
+            var div = $("<div>")
             $("<img>",{
-                src: cdnPath + "images/badge.png",
+                src: cdnPath + "images/counter.png",
                 "class": "countersback"
-            }).appendTo("body");
+            }).appendTo(div);
+            $("<span>", {"style": "position:relative; top:-20px; left:-50px;")
+                .text(count).appendTo(div)
+            div.appendTo("body")
         }
         $.getJSON(
             "http://countersback.herokuapp.com/hit?callback=?",
             {domain_id: config && config.domain_id},
             function(data){
                 if (this.data.count){
-                    addBadge(this.data.count);
+                    addBadge(this.data.count)
                 }
             }
         )
